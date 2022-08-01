f1dd81ca84b160a844dc811d68d7fc768092672a
ecelayes <ecelayes@gmail.com>
2022-08-01 00:02:10 -0300

    make it better

--- src/main/java/com/portfolio/backend/controller/SkillController.java
+++ src/main/java/com/portfolio/backend/controller/SkillController.java
@@ -9,7 +9,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +19,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/skill")
 public class SkillController {
     
